312f838660f53892ad0fbca868d9abec5aa4b03c
leeduhan <greenandsnow@gmail.com>
2019-10-22 10:08:39 +0900

    코드리뷰 추가

--- 4st/src/views/Homework.vue
+++ 4st/src/views/Homework.vue
@@ -42,7 +42,37 @@
     </div>
 </template>
 <style lang="scss" scoped>
-/* grid 안쓰고 레이아웃을 만드는게 첫번째 과제 입니다. grid는 나중에 쓸껍니다. */
+/* 
+1. grid 안쓰고 레이아웃을 만드는게 첫번째 과제 입니다. grid는 나중에 쓸껍니다. 
+2. css 클래스 역시 프로그래밍의 공통 개념은 같습니다. 단일로 선언 하는경우 는 공통 인경우 이고, 현재 레이아웃에 해당하는 경우는 적용 범위를 줄여야 합니다. 
+예> 
+.box-1{
+    height: 100px;
+    .box-1-sm {
+        width: 80vw;
+        height: 100px;
+        display: inline-block;
+    }
+}
+이렇게 하면 box-1-sm 의 적용범위를 .box-1로 제한 할수 있습니다. 
+또한 저렇게 모든 자식에게 적용하는 경우는 일일히 클래스를 주지 않아도 됩니다.
+기본 css 를 먼저 선언하고 좀 다른 애들만 클래스를 다르게 주어서 css 오버라이딩 개념을 사용해서 처리 합니다. 
+예>
+.box-1{
+    height: 100px;
+    > div {
+        width: 80vw;
+        height: 100px;
+        display: inline-block;
+        color: white;
+    }
+    .box-1-sm {
+        color: red;
+    }
+}
+
+이렇게 하면 우선적으로 .box-1 > div 가 먼저 적용되고 그에 해당하는 애들중 .box-1 .box-1-sm 이 있는경우 color 를 오버라이딩 하게 됩니다. 
+*/
 .ba-1 {
     border: 1px solid black;
 }
